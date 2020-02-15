4e03bc338eed7685db6d34daf2423776a46373e0
redeyesabs <56068337+redeyesabs@users.noreply.github.com>
2020-02-15 20:37:41 +0530

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+var app=angular.module('single-page-app', ['ngRoute']);
+
+
+app.config(function($routeProvider){
+
+
+      $routeProvider
+          .when('/',{
+                templateUrl: '/index.html'
+          })
+          .when('/about',{
+                templateUrl: 'aboutBody.html'
+          });
+
+});
